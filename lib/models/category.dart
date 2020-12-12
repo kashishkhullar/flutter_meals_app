164710dedf223ac623f3566890a7c5a5e88ea6f1
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-12 14:46:26 +0530

    "created category widget"

---
+++ lib/models/category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id, title;
+  final Color color;
+
+  const Category(
+      {@required this.id, @required this.title, @required this.color});
+}
