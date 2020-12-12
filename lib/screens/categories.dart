55a648ecdb3038928dad00bd4d0ffb8f20e63785
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-12 15:03:38 +0530

    "added dummy data to category screen"

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/dummy_data.dart';
+import 'package:flutter_meals_app/widgets/category.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES
+          .map((category) => CategoryItem(category.title, category.color))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
