6f490b6c16c2bc74abedbb0d7a5501dc1d242e2e
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-12 15:45:11 +0530

    "added navigation to category meals"

--- lib/widgets/category.dart
+++ lib/widgets/category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/screens/meals.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,21 +7,37 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return Meals();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // const optmizes the build process
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline1,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          color.withOpacity(0.7),
-          color,
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        borderRadius: BorderRadius.circular(15),
+    // ink well is same as gesture detector but provides the ripple effect on tap
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        // const optmizes the build process
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            color.withOpacity(0.7),
+            color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
